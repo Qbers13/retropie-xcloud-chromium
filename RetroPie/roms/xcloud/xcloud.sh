1d7ab150defa1a1b371af9ec45c6ad966adbabcc
Qbers13 <59139388+Qbers13@users.noreply.github.com>
2022-12-30 19:38:19 +0100

    change google-chrome to chromium-browser

--- RetroPie/roms/xcloud/xcloud.sh
+++ RetroPie/roms/xcloud/xcloud.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-google-chrome --start-fullscreen --start-maximized --app="https://www.xbox.com/en-US/play"
+chromium-browser --start-fullscreen --start-maximized --app="https://www.xbox.com/en-US/play"
